f1716537a175ba34e9fcc19e04b1de0a47672721
ty-a <tyler@faceyspacies.com>
2014-11-22 20:14:09 -0600

    Volume on exchange pages
    less use of exceptions, more module support

--- src/com/faceyspacies/GEMWbot/GrandExchangeUpdater.java
+++ src/com/faceyspacies/GEMWbot/GrandExchangeUpdater.java
@@ -14,8 +14,6 @@ import java.util.Iterator;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.zip.ZipException;
-
 import javax.security.auth.login.FailedLoginException;
 import javax.security.auth.login.LoginException;
 
@@ -29,6 +27,7 @@ public class GrandExchangeUpdater implements Runnable {
 	
 	private int numberOfPages;
 	private int numberOfPagesUpdated;
+	private int runningMode;
 	
 	private String wikiUserName;
 	private String wikiUserPass;
@@ -59,6 +58,8 @@ public class GrandExchangeUpdater implements Runnable {
 		wikiBot.setMarkBot(true);
 		wikiBot.setThrottle(0);
 		errorLog = "";
+		
+		runningMode = 1; // 0 = exchange, 1 = module, 2 = both
 		
 	}
 	
@@ -112,6 +113,7 @@ public class GrandExchangeUpdater implements Runnable {
 	
 	@Override
 	public void run() {
+
 		running = true;
 		try {
 			Start();
@@ -136,19 +138,40 @@ public class GrandExchangeUpdater implements Runnable {
 		if(volumes.isEmpty()) {
 			errorLog += "# Failed to get volume data due to connection issue; Attempted to continue\n";
 		}
+		
+		String[] pages;
+		
+		switch(runningMode) {
+		case 0: // template
+			pages = getPages(wikiBot);
+			break;
+		case 1: // module
+			pages = getModulePages(wikiBot);
+			break;
+		default: //both
+			pages = getPages(wikiBot);
+			if(pages == null) 
+				return;
+			String[] newPages = new String[pages.length];
+			for(int i = 0; i < pages.length; i++ ) {
+				newPages[i] = pages[i].replace("Exchange:", "");
+			}
+			pages = newPages;
+		}
 
-		String[] pages = getPages(wikiBot);
 		if(pages == null) { // abort, message was sent in function
 			return;
 		}
 		numberOfPages = pages.length;
 		numberOfPagesUpdated = 0;
 		
-		 for(int i = 0; i < pages.length; i++) {
-		 
+		for(int i = 0; i < pages.length; i++) {
+		
 			// update the pages, once it is done add to log
 			if(running) {
 				numberOfPagesUpdated++;
+				if(pages[i].contains("/Data")) // we update /Data pages as part of updating the main page
+					continue;
 				addToLog(doUpdates(pages[i]), pages[i]);
 			
 				try {
@@ -179,76 +202,6 @@ public class GrandExchangeUpdater implements Runnable {
 		ircInstance.sendMessage(ircChannel, "GE Updates complete!"); // if we make it to end, we did it
 	}
 	
-	private void updateLogPage() {
-		try {
-			if(errorLog.equalsIgnoreCase("")) 
-				errorLog = "No issues today; yay!";
-			wikiBot.newSection(logPage, "{{subst:CURRENTDAY}} {{subst:CURRENTMONTHNAME}} {{subst:CURRENTYEAR}}", errorLog, false, true);
-		} catch (LoginException | IOException e) {
-			ircInstance.sendMessage(ircChannel, "`tell @wikia/vstf/TyA could not save the log page; that is just great.");
-		}
-		
-	}
-	
-	private void addToLog(UpdateResult status, String pageName) {
-		if(!status.getSuccess()) {
-			errorLog += "# " + pageName + " - " + status.getResult() + "\n"; 
-		}
-	}
-
-	private UpdateResult doUpdates(String page) {
-		int failures;
-		failures = 0;
-		while(failures < 3) {
-			try {
-				updatePage(page);
-				return new UpdateResult("", true);
-			}
-			catch (PageIsEmptyException e) {
-				return new UpdateResult("page is empty", false); // cannot recover
-			}
-			catch (NoItemIDException e) {
-				return new UpdateResult("no item id", false); // cannot recover
-			} catch (IOException e) {
-				failures++;
-				if(failures == 3) {
-					return new UpdateResult("network failure", false);
-				}
-				
-			} catch (LoginException e) {
-				failures++;
-				if(failures == 3) {
-					return new UpdateResult("failed to stay logged in", false);
-				}
-				Login();
-			}
-		}
-		
-		return new UpdateResult("unknown error", false);
-	}
-	
-	private String[] getPages(Wiki wikiBot) {
-		
-		int failures = 0;
-		
-		while(failures < 3) {
-			try {
-				// test wiki does not have namespace 112, but main wiki does
-				//String[] pages = wikiBot.getCategoryMembers("Grand Exchange", 112);
-				String[] pages = wikiBot.getCategoryMembers("Grand Exchange");
-				return pages;
-			} catch (IOException e1) {
-				failures++;
-				if(failures == 3) {
-					ircInstance.sendMessage(ircChannel, "`tell @wikia/vstf/TyA unable to get page list.");
-					return null;
-				}
-			}
-		}
-		
-		return null; // shouldn't reach here, but if we do assume we have failed. 
-	}
-	
 	private boolean Login() {
 		int failures = 0;
 		while(failures < 3) {
@@ -277,7 +230,85 @@ public class GrandExchangeUpdater implements Runnable {
 		return false;
 	}
 	
-	private boolean updatePage(String pageName) throws PageIsEmptyException, NoItemIDException, IOException, ZipException, LoginException {
+	private void updateLogPage() {
+		try {
+			if(errorLog.equalsIgnoreCase("")) 
+				errorLog = "No issues today; yay!";
+			wikiBot.newSection(logPage, "{{subst:CURRENTDAY}} {{subst:CURRENTMONTHNAME}} {{subst:CURRENTYEAR}}", errorLog, false, true);
+		} catch (LoginException | IOException e) {
+			ircInstance.sendMessage(ircChannel, "`tell @wikia/vstf/TyA could not save the log page; that is just great.");
+		}
+		
+	}
+	
+	private void addToLog(UpdateResult status, String pageName) {
+		if(!status.getSuccess()) {
+			errorLog += "# [[" + pageName.replace("_", " ") + "]] - " + status.getResult() + "\n"; 
+		}
+	}
+
+	private UpdateResult doUpdates(String page) {
+		int failures;
+		failures = 0;
+		
+		while(failures < 3) {
+			try {
+				// all items that do not throw exceptions are not recoverable
+				switch(runningMode) {
+				case 0:
+					return updatePage(page);
+				case 1:
+					return updateModulePage(page);
+				case 2:
+					addToLog(updatePage(page), page);
+					return updateModulePage(page);
+				}
+			} catch (IOException e) {
+				failures++;
+				if(failures == 3) {
+					return new UpdateResult("network failure", false);
+				}
+				
+			} catch (LoginException e) {
+				failures++;
+				if(failures == 3) {
+					return new UpdateResult("failed to stay logged in", false);
+				}
+				Login();
+			}
+		}
+		
+		return new UpdateResult("unknown error", false);
+	}
+	
+	private String[] getPages(Wiki wikiBot) {
+		
+		int failures = 0;
+		String[] page = new String[1];
+		page[0] = "Exchange:Iron_bar";
+		return page;
+		/*
+		
+		while(failures < 3) {
+			try {
+				// test wiki does not have namespace 112, but main wiki does
+				//String[] pages = wikiBot.getCategoryMembers("Grand Exchange", 112);
+				String[] pages = wikiBot.getCategoryMembers("Grand Exchange");
+				return pages;
+			} catch (IOException e1) {
+				failures++;
+				if(failures == 3) {
+					ircInstance.sendMessage(ircChannel, "`tell @wikia/vstf/TyA unable to get page list.");
+					return null;
+				}
+			}
+		} 
+		
+		return null; // shouldn't reach here, but if we do assume we have failed. 
+		*/
+	}
+	
+	private UpdateResult updatePage(String pageName) throws IOException, LoginException {
 		String itemID = null;
 		GEPrice newPrice = null;
 		
@@ -287,13 +318,16 @@ public class GrandExchangeUpdater implements Runnable {
 			pageName = "Exchange:" + pageName;
 		}
 		
-		//System.out.println(pageName);
+		boolean doesExist;
+		doesExist = (boolean)wikiBot.getPageInfo(pageName).get("exists");
+		if(!doesExist) {
+			return new UpdateResult("page does not exist", false);
+		}
 		
 		String pageContent = wikiBot.getPageText(pageName);
-		//System.out.println(pageContent);
 		
 		if(pageContent.length() == 0) {
-			throw new PageIsEmptyException();
+			return new UpdateResult("page is empty", false);
 		}
 		
 		Pattern itemIDregex = Pattern.compile("\\|ItemId=(\\d+)");
@@ -301,20 +335,36 @@ public class GrandExchangeUpdater implements Runnable {
 		
 		if(itemIDMatcher.find()) {
 			itemID = itemIDMatcher.group(1);
-			//System.out.println(itemID);
 		} else {
 			System.out.println("[ERROR] unable to load item id");
-			throw new NoItemIDException();
+			return new UpdateResult("unable to load item id", false);
+
 		}
 		
 		try {
 			newPrice = loadCurPrice(itemID);
 		} catch (MalformedURLException e) {
 			System.out.println("[ERROR] Item ID " + itemID + " is invalid!");
+			return new UpdateResult("item id is invalid", false);
 		}
 		
 		if(newPrice == null)
-			return false;
+			return new UpdateResult("unable to fetch price", false);
+		
+		String volume = volumes.getVolumeFor(newPrice.getId());
+		if(volume != null) { // WE HAVE VOLUME DATA, WOO!
+			// since we don't need the data again, it is safe to just remove it
+			if(pageContent.indexOf("Volume=") != -1) {
+				pageContent = pageContent.replaceAll("\\|Volume=.*\\n", "\\|Volume=" + volume + "\n");
+				pageContent = pageContent.replaceAll("\\|VolumeDate=.*\\n", "\\|VolumeDate=~~~~~\n");
+			} else { // Does not have volume fields on page, but has data. probs newly on top100
+				String newVolText = "";
+				newVolText = "|Volume= " + volume + "\n";
+				newVolText += "|VolumeData=~~~~~\n";
+				newVolText += "|Limit=";
+				pageContent = pageContent.replaceAll("\\|Limit=", newVolText);
+			}
+		}
 		
 		// remove data that we don't need
 		pageContent = pageContent.replaceAll("\\|Last=.*\\n", "");
@@ -328,48 +378,32 @@ public class GrandExchangeUpdater implements Runnable {
 		
 		wikiBot.edit(pageName, pageContent, "Updating [[" + pageName.replace("_", " ")  + "]]");
 		
-		addToLog(updateData(pageName, newPrice), pageName + "/Data");
-		return true;
+		addToLog(doDataUpdate(pageName, newPrice), pageName + "/Data");
+		return new UpdateResult("", true);
 	}
 	
-	private UpdateResult updateData(String pageName, GEPrice price) {
-		pageName = pageName + "/Data";
-		String pageContent;
-		int failures = 0;
+	private UpdateResult doDataUpdate(String pageName, GEPrice price) {
+		int failures;
+		failures = 0;
+		pageName += "/Data";
+		
 		while(failures < 3) {
 			try {
-				pageContent = wikiBot.getPageText(pageName);
+				// all items that do not throw exceptions are not recoverable
 				
-				if(pageContent.length() == 0) {
-					throw new PageIsEmptyException();
-				}
-				
-				pageContent = pageContent.replaceAll("\\n}}", ",");
-				
-				String volume = volumes.getVolumeFor(price.getId());
-				if(volume == null) {
-					pageContent += price.getTimestamp() + ":" + price.getPrice() + "\n}}";
+				if(pageName.indexOf("Exchange:") == -1) { // module
+					return updateModuleData(pageName, price);
 				} else {
-					pageContent += price.getTimestamp() + ":" + price.getPrice() + ":" + volume  + "\n}}";
+					return updateData(pageName, price);
 				}
-			
-				wikiBot.edit(pageName, pageContent, "Updating [[" + pageName.replace("_", " ") + "]]");
 				
-				return new UpdateResult("", true);
-				
-				} 
-			
-			catch (PageIsEmptyException e) {
-				return new UpdateResult("page is empty", false); // cannot recover
-			} 
-			catch (IOException e) {
+			} catch (IOException e) {
 				failures++;
 				if(failures == 3) {
 					return new UpdateResult("network failure", false);
 				}
 				
-			} 
-			catch (LoginException e) {
+			} catch (LoginException e) {
 				failures++;
 				if(failures == 3) {
 					return new UpdateResult("failed to stay logged in", false);
@@ -377,6 +411,36 @@ public class GrandExchangeUpdater implements Runnable {
 				Login();
 			}
 		}
+		
+		return new UpdateResult("unknown error", false);
+	}
+	
+	private UpdateResult updateData(String pageName, GEPrice price) throws LoginException, IOException {
+		String pageContent;		
+		boolean doesExist;
+		
+		doesExist = (boolean) wikiBot.getPageInfo(pageName).get("exists");
+		if(!doesExist) {
+			return new UpdateResult("page does not exist", false);
+		}
+		
+		pageContent = wikiBot.getPageText(pageName);
+		
+		if(pageContent.length() == 0) {
+			return new UpdateResult("page is empty", false);
+			
+		}
+		
+		pageContent = pageContent.replaceAll("\\n}}", ",");
+		
+		String volume = volumes.getVolumeFor(price.getId());
+		if(volume == null) {
+			pageContent += price.getTimestamp() + ":" + price.getPrice() + "\n}}";
+		} else {
+			pageContent += price.getTimestamp() + ":" + price.getPrice() + ":" + volume  + "\n}}";
+		}
+		
+		wikiBot.edit(pageName, pageContent, "Updating [[" + pageName.replace("_", " ") + "]]");
 		
 		return new UpdateResult("", true);
 	}
@@ -400,7 +464,7 @@ public class GrandExchangeUpdater implements Runnable {
 		return null;
 	}
 	
-	private boolean updateModulePage(String pageName) throws PageIsEmptyException, NoItemIDException, IOException, ZipException, LoginException {
+	private UpdateResult updateModulePage(String pageName) throws IOException, LoginException {
 		String itemID = null;
 		GEPrice newPrice = null;
 		
@@ -410,13 +474,10 @@ public class GrandExchangeUpdater implements Runnable {
 			pageName = "Module:Exchange/" + pageName;
 		}
 		
-		//System.out.println(pageName);
-		
 		String pageContent = wikiBot.getPageText(pageName);
-		//System.out.println(pageContent);
 		
 		if(pageContent.length() == 0) {
-			throw new PageIsEmptyException();
+			return new UpdateResult("page is empty", false);
 		}
 		
 		Pattern itemIDregex = Pattern.compile("itemId\\s*= (\\d+)");
@@ -424,20 +485,27 @@ public class GrandExchangeUpdater implements Runnable {
 		
 		if(itemIDMatcher.find()) {
 			itemID = itemIDMatcher.group(1);
-			System.out.println(itemID);
 		} else {
 			System.out.println("[ERROR] unable to load item id");
-			throw new NoItemIDException();
+			return new UpdateResult("unable to load item id", false);
 		}
 		
 		try {
 			newPrice = loadCurPrice(itemID);
 		} catch (MalformedURLException e) {
 			System.out.println("[ERROR] Item ID " + itemID + " is invalid!");
+			return new UpdateResult("item id is invalid", false);
 		}
 		
 		if(newPrice == null)
-			return false;
+			return new UpdateResult("unable to fetch price", false);
+		
+		String volume = volumes.getVolumeFor(newPrice.getId());
+		if(volume != null) { // WE HAVE VOLUME DATA, WOO!
+			// since we don't need the data again, it is safe to just remove it
+			pageContent = pageContent.replaceAll("volume\\s*=.*\\n", "volume     = " + volume + ",\n");
+			pageContent = pageContent.replaceAll("volumeDate\\s*=.*\\n", "volumeDate = '~~~~~',\n");
+		}
 		
 		// remove data that we don't need
 		pageContent = pageContent.replaceAll("\\s*last\\s*=.*\n", "\n");
@@ -450,56 +518,29 @@ public class GrandExchangeUpdater implements Runnable {
 		
 		wikiBot.edit(pageName, pageContent, "Updating [[" + pageName.replace("_", " ")  + "]]");
 		
-		addToLog(updateModuleData(pageName, newPrice), pageName + "/Data");
-		return true;
+		addToLog(doDataUpdate(pageName, newPrice), pageName + "/Data");
+		return new UpdateResult("", true);
 	}
 	
-	private UpdateResult updateModuleData(String pageName, GEPrice price) {
-		pageName = pageName + "/Data";
+	private UpdateResult updateModuleData(String pageName, GEPrice price) throws LoginException, IOException {
 		String pageContent;
-		int failures = 0;
-		while(failures < 3) {
-			try {
-				pageContent = wikiBot.getPageText(pageName);
-				
-				if(pageContent.length() == 0) {
-					throw new PageIsEmptyException();
-				}
-				
-				pageContent = pageContent.replaceAll("\\n}", ",");
-				
-				String volume = volumes.getVolumeFor(price.getId());
-				if(volume == null) {
-					pageContent += "'" + price.getTimestamp() + ":" + price.getPrice() + "'\n}";
 
-				} else {
-					pageContent += "'" + price.getTimestamp() + ":" + price.getPrice() + ":" + volume + "'\n}";
-				}
-
-				wikiBot.edit(pageName, pageContent, "Updating [[" + pageName.replace("_", " ") + "]]");
-				
-				return new UpdateResult("", true);
-				
-			} 
-			
-			catch (PageIsEmptyException e) {
-				return new UpdateResult("page is empty", false); // cannot recover
-			} 
-			catch (IOException e) {
-				failures++;
-				if(failures == 3) {
-					return new UpdateResult("network failure", false);
-				}
-				
-			} 
-			catch (LoginException e) {
-				failures++;
-				if(failures == 3) {
-					return new UpdateResult("failed to stay logged in", false);
-				}
-				Login();
-			}
+		pageContent = wikiBot.getPageText(pageName);
+		
+		if(pageContent.length() == 0) {
+			return new UpdateResult("page is empty", false);
 		}
+		
+		pageContent = pageContent.replaceAll("\\n}", ",");
+		
+		String volume = volumes.getVolumeFor(price.getId());
+		if(volume == null) {
+			pageContent += "    '" + price.getTimestamp() + ":" + price.getPrice() + "'\n}";
+		} else {
+			pageContent += "    '" + price.getTimestamp() + ":" + price.getPrice() + ":" + volume + "'\n}";
+		}
+
+		wikiBot.edit(pageName, pageContent, "Updating [[" + pageName.replace("_", " ") + "]]");
 		
 		return new UpdateResult("", true);
 	}
